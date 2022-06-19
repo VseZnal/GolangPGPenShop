4a800c6acb283d6161f4a95855913f4bac2e39eb
VseZnal <Vseznal66@gmail.com>
2022-06-20 00:07:20 +0300

    up

--- main.go
+++ main.go
@@ -1,16 +1,132 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	r := gin.Default()
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Item{})
+
+	handler := newHandler(db)
+
+	r := gin.New()
+
+	r.POST("/login", loginHandler)
+
+	protected := r.Group("/", authorizationMiddleware)
+
+	protected.GET("/items", handler.listItemsHandler)
+	protected.POST("/items", handler.createItemHandler)
+	protected.DELETE("/items/:id", handler.deleteItemHandler)
 
 	r.Run()
+}
+
+type Handler struct {
+	db *gorm.DB
+}
+
+func newHandler(db *gorm.DB) *Handler {
+	return &Handler{db}
+}
+
+type Item struct {
+	ID          uint   `json:"id" gorm:"primary_key"`
+	Title       string `json:"title"`
+	Description string `json:"Description"`
+	Contact     string `json:"Contact"`
+	Phone       string `json:"Phone"`
+}
+
+func authorizationMiddleware(c *gin.Context) {
+	s := c.Request.Header.Get("Authorization")
+
+	token := strings.TrimPrefix(s, "Bearer ")
+
+	if err := validateToken(token); err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+}
+
+func validateToken(token string) error {
+	_, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte("MySignature"), nil
+	})
+
+	return err
+}
+
+func loginHandler(c *gin.Context) {
+	// login logic here
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(5 * time.Minute).Unix(),
+	})
+
+	ss, err := token.SignedString([]byte("MySignature"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": ss,
+	})
+}
+
+func (h *Handler) listItemsHandler(c *gin.Context) {
+	var items []Item
+
+	if result := h.db.Find(&items); result.Error != nil {
+		return
+	}
+
+	c.JSON(http.StatusOK, &items)
+}
+
+func (h *Handler) createItemHandler(c *gin.Context) {
+	var item Item
+
+	if err := c.BindJSON(&item); err != nil {
+		return
+	}
+
+	if result := h.db.Create(&item); result.Error != nil {
+		return
+	}
+
+	c.JSON(http.StatusCreated, &item)
+}
+
+func (h *Handler) deleteItemHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	if result := h.db.Delete(&Item{}, id); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
--- models/item.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Item struct {
-	ID          uint   `json:"id" gorm:"primary_key"`
-	Title       string `json:"title"`
-	Description string `json:"Description"`
-	Contact     string `json:"Contact"`
-	Phone       string `json:"Phone"`
-}
